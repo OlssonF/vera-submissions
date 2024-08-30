aecd167c50e9b18a714b70a78374a841a0d0c56c
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2024-08-30 10:32:10 -0400

    fix

--- Models/convert_to_physical_metrics.R
+++ Models/convert_to_physical_metrics.R
@@ -25,7 +25,7 @@ for (i in 1:length(models)) {
   
   # is that file present in the bucket?
   for (date in 1:nrow(this_year)) {
-    this_year$exists[date] <- this_year$date[date] %in% submissions
+    this_year$exists[date] <- this_year$date[date] %in% ss_submissions
   }
   
   
@@ -52,63 +52,65 @@ for (i in 1:length(models)) {
       message(paste0("This forecast date or model doesn't exist - ",
                      paste0("project_id=vera4cast/duration=P1D/variable=Temp_C_mean/model_id=",
                             forecast_model_id, ": reference_date = ", forecast_date)))
+    } else {
+      
+      sites <- distinct(t_forecast, site_id) |> pull()
+      
+      rla_output <- NULL
+      
+      for(j in 1:length(sites)) {
+        # Read in bathymetry
+        bathy <- read.csv("https://pasta.lternet.edu/package/data/eml/edi/1254/1/f7fa2a06e1229ee75ea39eb586577184",
+                          header=F,skip=1,sep=",",col.names=c("Reservoir",     
+                                                              "Depth_m",     
+                                                              "SA_m2",     
+                                                              "Volume_layer_L",     
+                                                              "Volume_below_L"), check.names=TRUE) |> 
+          filter(Reservoir %in% c('FCR', 'BVR')) |> 
+          mutate(Reservoir = ifelse(Reservoir == 'FCR', 'fcre', 'bvre')) |> 
+          select(Reservoir, Depth_m, SA_m2) |> 
+          rename(site_id = Reservoir, 
+                 depths = Depth_m,
+                 areas = SA_m2) |> 
+          filter(site_id == sites[j]) |> 
+          select(-site_id)
+        
+        # Calculate the forecasted schmidt stbaility for each ensemble member
+        message("Generating forecast...")
+        schmidt <- t_forecast |> 
+          split(t_forecast$parameter) |> 
+          map(~calculate_schmidt(., bathy= bathy)) |> 
+          list_rbind(names_to = 'parameter') |> 
+          mutate(site_id = sites[j])
+        
+        # And then thermocline depth
+        thermo <- t_forecast |> 
+          split(t_forecast$parameter) |> 
+          map(~calculate_thermocline(., Smin = 0.1)) |> 
+          list_rbind(names_to = 'parameter') |> 
+          mutate(site_id = sites[j])
+        
+        rla_output <- bind_rows(list(rla_output, schmidt, thermo)) 
+      }
+      
+      # Format the output for a forecast and save
+      forecast <- rla_output |> 
+        mutate(model_id = forecast_model_id,
+               project_id = 'vera4cast',
+               duration = 'P1D',
+               depth_m = NA,
+               family = 'ensemble') 
+      
+      forecast_filename <- file.path('Forecasts', paste0('physical-metrics-', forecast_date, '-', forecast_model_id, '.csv'))
+      write_csv(forecast, forecast_filename)
+      # vera4castHelpers::forecast_output_validator(forecast_filename)
+      vera4castHelpers::submit(forecast_filename)
+      
+      
+    }
+      
     }
     
-    sites <- distinct(t_forecast, site_id) |> pull()
-    
-    rla_output <- NULL
-    
-    for(j in 1:length(sites)) {
-      # Read in bathymetry
-      bathy <- read.csv("https://pasta.lternet.edu/package/data/eml/edi/1254/1/f7fa2a06e1229ee75ea39eb586577184",
-                        header=F,skip=1,sep=",",col.names=c("Reservoir",     
-                                                            "Depth_m",     
-                                                            "SA_m2",     
-                                                            "Volume_layer_L",     
-                                                            "Volume_below_L"), check.names=TRUE) |> 
-        filter(Reservoir %in% c('FCR', 'BVR')) |> 
-        mutate(Reservoir = ifelse(Reservoir == 'FCR', 'fcre', 'bvre')) |> 
-        select(Reservoir, Depth_m, SA_m2) |> 
-        rename(site_id = Reservoir, 
-               depths = Depth_m,
-               areas = SA_m2) |> 
-        filter(site_id == sites[j]) |> 
-        select(-site_id)
-      
-      # Calculate the forecasted schmidt stbaility for each ensemble member
-      message("Generating forecast...")
-      schmidt <- t_forecast |> 
-        split(t_forecast$parameter) |> 
-        map(~calculate_schmidt(., bathy= bathy)) |> 
-        list_rbind(names_to = 'parameter') |> 
-        mutate(site_id = sites[j])
-      
-      # And then thermocline depth
-      thermo <- t_forecast |> 
-        split(t_forecast$parameter) |> 
-        map(~calculate_thermocline(., Smin = 0.1)) |> 
-        list_rbind(names_to = 'parameter') |> 
-        mutate(site_id = sites[j])
-      
-      rla_output <- bind_rows(list(rla_output, schmidt, thermo)) 
-    }
-    
-    # Format the output for a forecast and save
-    forecast <- rla_output |> 
-      mutate(model_id = forecast_model_id,
-             project_id = 'vera4cast',
-             duration = 'P1D',
-             depth_m = NA,
-             family = 'ensemble') 
-    
-    forecast_filename <- file.path('Forecasts', paste0('physical-metrics-', forecast_date, '-', forecast_model_id, '.csv'))
-    write_csv(forecast, forecast_filename)
-    # vera4castHelpers::forecast_output_validator(forecast_filename)
-    vera4castHelpers::submit(forecast_filename)
-    
-  
-  }
-  
   
   
 }
