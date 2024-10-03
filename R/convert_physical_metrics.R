3be553112599c38926cb9fe372e9b8bbdf3fc4b2
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2024-10-03 12:16:26 -0400

    function to calculate mixed binary

--- R/convert_physical_metrics.R
+++ R/convert_physical_metrics.R
@@ -61,3 +61,36 @@ calculate_thermocline <- function(temp_forecast, # a formatted temperature forec
   
   return(thermo)
 }
+
+calculate_mixed_binary <- function(temp_forecast, # a formatted temperature forecast with at least  
+                                  # the datetime,ref_date, depth_m, and prediction columns
+                                  Smin = 0.1) { # density difference cutoff
+  
+  # check this is a single forecast
+  check_forecast <- temp_forecast |> 
+    distinct(parameter, reference_datetime, site_id)
+  
+  if (nrow(check_forecast) > 1) {
+    stop('This is not a single forecast. Should not have multiple sites, reference_dates or ensemble members')
+  }
+  
+  depths <-  temp_forecast |>
+    dplyr::summarise(top = min(as.numeric(depth_m)),
+                     bottom = max(as.numeric(depth_m)))  |>
+    tidyr::pivot_longer(cols = top:bottom,
+                        values_to = 'depth_m', names_to = 'location') 
+  
+  mixed <- temp_forecast |>
+    dplyr::select(datetime, depth_m, prediction) |> 
+    dplyr::inner_join(depths, by = join_by(depth_m)) |> 
+    dplyr::mutate(prediction = rLakeAnalyzer::water.density(prediction)) |> 
+    tidyr::pivot_wider(names_from = location, values_from = prediction, id_cols = datetime) |> 
+    dplyr::mutate(density_diff = bottom - top,
+                  prediction = ifelse(abs(density_diff) < Smin, 1, 0)) |> #mixed = 1, strat = 0
+    dplyr::arrange(datetime) |> 
+    dplyr::mutate(reference_datetime = unique(temp_forecast$reference_datetime), 
+           variable =  "Mixed_binary_mean") |> 
+    dplyr::select(datetime, reference_datetime, prediction, variable)
+  
+  return(mixed)
+}
