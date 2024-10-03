2a58ce0c4f77fa3d2e60a5f97b2788b5b0a77d56
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2024-10-03 11:24:24 -0400

    add aed forecast and switch to grab from flare bucket

--- Models/convert_to_physical_metrics.R
+++ Models/convert_to_physical_metrics.R
@@ -3,15 +3,18 @@ source("R/convert_physical_metrics.R")
 
 install.packages('rLakeAnalyzer')
 # Which models can we calculate the metrics for?
-models <- c('flareGOTM', 'flareSimstrat', 'glm_aed_v1')
+flare_models <- c('GOTM', 'Simstrat', 'glm_aed_v1', 'glm_aed_flare_v3')
+vera_models <- c('flareGOTM', 'flareSimstrat', 'glm_aed_v1', 'glm_aed_flare_v3')
 
+sites <- 'fcre'
 
-for (i in 1:length(models)) {
+for (i in 1:length(flare_models)) {
   # check for any missing forecasts first!
-  message(models[i])
+  message(flare_models[i])
   message("Checking for missed forecasts...")
   
-  forecast_model_id <- models[i]
+  forecast_model_id <- flare_models[i]
+  submission_model_id <- vera_models[i]
   
   # Dates of forecasts
   today <- paste(Sys.Date() - days(1), '00:00:00')
@@ -20,7 +23,7 @@ for (i in 1:length(models)) {
   
   # what forecasts have already been submitted?
   ss_submissions <- arrow::open_dataset(paste0("s3://anonymous@bio230121-bucket01/vera4cast/forecasts/parquet/project_id=vera4cast/duration=P1D/variable=SchmidtStability_Jm2_mean/model_id=",
-                                               forecast_model_id, 
+                                               submission_model_id, 
                                                "?endpoint_override=renc.osn.xsede.org")) |> 
     distinct(reference_date) |> pull(as_vector = T)
   
@@ -42,24 +45,26 @@ for (i in 1:length(models)) {
     
     forecast_date <- missed_dates[n]
     message("Getting temperature forecast")
-    # Get the temperature forecast that was submitted to vera
-    t_forecast <- arrow::open_dataset(paste0("s3://anonymous@bio230121-bucket01/vera4cast/forecasts/parquet/project_id=vera4cast/duration=P1D/variable=Temp_C_mean/model_id=",
-                                             forecast_model_id, 
+    # Get the temperature forecast that was submitted to vera bio230121-bucket01/flare/forecasts/parquet
+    t_forecast <- arrow::open_dataset(paste0("s3://anonymous@bio230121-bucket01/flare/forecasts/parquet/",
                                              "?endpoint_override=renc.osn.xsede.org")) |>
-      filter(reference_date == forecast_date) |> 
-      dplyr::collect()
+      filter(reference_date == forecast_date,
+             site_id %in% sites,
+             model_id == forecast_model_id,
+             variable %in% c('temperature','Temp_C_mean')) |> # variable name is different for LER models
+      dplyr::collect() |> 
+      rename(depth_m = depth)
     
     if (nrow(t_forecast) < 1) {
-      message(paste0("This forecast date or model doesn't exist - ",
-                     paste0("project_id=vera4cast/duration=P1D/variable=Temp_C_mean/model_id=",
-                            forecast_model_id, ": reference_date = ", forecast_date)))
+      message(paste0("This forecast date or model doesn't exist: ",
+                     paste0("model_id=", forecast_model_id, "/reference_date=", forecast_date)))
     } else {
       
-      sites <- distinct(t_forecast, site_id) |> pull()
+      mod_sites <- distinct(t_forecast, site_id) |> pull()
       
       rla_output <- NULL
       
-      for(j in 1:length(sites)) {
+      for(j in 1:length(mod_sites)) {
         # Read in bathymetry
         bathy <- read.csv("https://pasta.lternet.edu/package/data/eml/edi/1254/1/f7fa2a06e1229ee75ea39eb586577184",
                           header=F,skip=1,sep=",",col.names=c("Reservoir",     
@@ -96,13 +101,13 @@ for (i in 1:length(models)) {
       
       # Format the output for a forecast and save
       forecast <- rla_output |> 
-        mutate(model_id = forecast_model_id,
+        mutate(model_id = submission_model_id,
                project_id = 'vera4cast',
                duration = 'P1D',
                depth_m = NA,
                family = 'ensemble') 
       
-      forecast_filename <- paste0('physical-metrics-', forecast_date, '-', forecast_model_id, '.csv')
+      forecast_filename <- paste0('physical-metrics-', forecast_date, '-', submission_model_id, '.csv')
       write_csv(forecast, forecast_filename)
       # vera4castHelpers::forecast_output_validator(forecast_filename)
       vera4castHelpers::submit(forecast_filename)
